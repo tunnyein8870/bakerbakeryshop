87c8784eac9d2ef55e1f4169e5e0e8f7864e2236
unknown <emcedm2018.gmail.com>
2022-03-30 01:19:59 +0630

    2 dashboard

--- exampleadmin.php
+++ exampleadmin.php
@@ -1,197 +1,36 @@
-<?php
-include('connection.php');
-include('header.php');
-$total = 0;
-$price = 0;
-$qty = 1;
-if (isset($_POST['remove'])) {
-  if ($_GET['action'] == 'remove') {
-    foreach ($_SESSION['cart'] as $key => $value) {
-      if ($value["pid"] == $_GET['id']) {
-        unset($_SESSION['cart'][$key]);
-        echo "<script>alert('Product has been Removed...!')</script>";
-      }
-    }
-  }
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
+<!DOCTYPE HTML>
+<html>
 <head>
-  <title>Baker Bakery Cart Page</title>
-  <link rel="stylesheet" type="text/css" href="css files/cart.css">
+<script type="text/javascript">
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light1", // "light2", "dark1", "dark2"
+	animationEnabled: true, // change to true		
+	title:{
+		text: "Basic Column Chart"
+	},
+	data: [
+	{
+		// Change type to "bar", "area", "spline", "pie",etc.
+		type: "column",
+		dataPoints: [
+			{ label: "apple",  y: 10  },
+			{ label: "orange", y: 15  },
+			{ label: "banana", y: 25  },
+			{ label: "mango",  y: 30  },
+			{ label: "grape",  y: 28  }
+		]
+	}
+	]
+});
+chart.render();
+
+}
+</script>
 </head>
-
 <body>
-  <!-- start of cart  -->
-  <!-- <section class="vh-100"> -->
-  <div class="startcart">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col">
-          <p><span class="h2"><br><b>&nbsp; Shopping Cart</b></span>
-            <!--           <span class="h4">(1 item in your cart)</span> -->
-          </p>
-          <div class="card mb-4">
-            <div class="card-body p-4">
-
-              <div class="row align-items-center">
-                <div class="col-md-2">
-                </div>
-                <div class="col-md-2 d-flex justify-content-center">
-                  <div>
-                    <p class="small text-muted mb-4 pb-2">Name</p>
-                  </div>
-                </div>
-                <div class="col-md-1 d-flex justify-content-center">
-                  <div>
-                    <p class="small text-muted mb-4 pb-2">Category</p>
-                  </div>
-                </div>
-                <div class="col-md-2 d-flex justify-content-center">
-                  <div>
-                    <p class="small text-muted mb-4 pb-2">Product Price</p>
-                  </div>
-                </div>
-                <div class="col-md-2 d-flex justify-content-center">
-                  <div>
-                    <p class="small text-muted mb-4 pb-2">Quantity</p>
-                  </div>
-                </div>
-                <div class="col-md-2 d-flex justify-content-center">
-                  <div>
-                    <p class="small text-muted mb-4 pb-2">Total Price</p>
-                  </div>
-                </div>
-                <div class="col-md-1 d-flex justify-content-center">
-                  <div>
-                    <p class="small text-muted mb-4 pb-2">Action</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <?php
-          if (isset($_SESSION['cart'])) {
-            $productid = array_column($_SESSION['cart'], 'pid');
-            $counter=1;
-                        include "connection.php";
-                        foreach ($productid as $id) {
-                        $counter++;
-                        $$counter = (int)$_POST[$counter];
-                        //echo "${$counter}";
-                        $viewquery = "SELECT * from product where pid='$id'";
-                        foreach ($dbconnection->query($viewquery) as $row){
-
-                          echo "<form class='updateform' method='post'>";
-                          echo "<div class='form-group'>";
-                          echo "<label for='exampleInputEmail1'>Product ID</label>";
-                          echo "<input class='form-control' type='text' name='pid' value='".$row['pid']."'>";
-                          echo "</div>";
-                          echo "<div class='form-group'>";
-                          echo "<label for='exampleInputEmail1'>Product Name</label>";
-                          echo "<input class='form-control' type='text' name='pname' value='".$row['pname']."'>";
-                          echo "</div>";
-                          echo "<div class='form-group'>";
-                          echo "<label for='exampleInputEmail1'>Product categories</label>";
-                          echo "<input class='form-control' type='text' name='pcat' value='".$row['categories']."' required>";
-                          echo "</div>";
-                          echo "<div class='form-group'>";
-                          echo "<label for='exampleInputEmail1'>Quantity</label>";
-                          echo "<input class='form-control' type='number' min='1' name='$counter' value='${$counter}'";
-                          echo "</div>";
-                          echo "<div class='form-group'>";
-                          echo "<label for='exampleInputEmail1'>Product Price</label>";
-                          echo "<input class='form-control' type='text' name='ppri' value='".$row['pprice']."'>";
-                          echo "</div>";
-                          
-                          echo "<div class='form-group'>";
-                          echo "<div class='text-center'>";
-                          echo "<button class='btn btn-success btn-block' type='submit' name='Confirm'>Confirm Quantity</button>";
-                          echo "</div>";
-                          
-                          $price = $row['pprice'];
-                          $price = ${$counter}*(int)$price;
-
-                          
-                          echo "<div class='form-group'>";
-                          echo "<label for='exampleInputEmail1'>Total Price</label>";
-                          echo "<input class='form-control' type='text' name='ppri' value='$price'>";
-                          echo "</div>";
-                          echo "</form>";
-                          //$counter=${$counter};
-                          }
-                      }              
-          } else {
-            echo "cart is empty";
-          }
-          ?>
-
-
-          <div class="card mb-5">
-            <div class="card-body p-4">
-
-              <div class="float-end">
-                <p class="mb-0 me-5 d-flex align-items-center">
-                  <span class="small text-muted me-2">Order total:</span> <span class="total lead fw-normal" id="total"><?php echo $total ?></span><span>Ks</span>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="d-flex justify-content-end">
-            <a href="shop.html" class="sh">&lt;&nbsp; Continue Shopping</a>
-            <form method="post">
-              <button type="submit" name='checkout' class="btn btn-primary btn-lg">Checkout</button>
-            </form>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--end startcart div -->
-  <!-- </section> -->
-
-  <!-- start of contact us -->
-  <div id="contact">
-    <h2>Contact Our Store
-      <hr>
-    </h2>
-    <div class="row">
-      <div class="col-sm-4">
-        <h3><strong>Have a question?</strong></h3><br>
-        <p>We're always here to lend a helping hand.</p><br>
-        <p>We also have physical store for you to visit.</p><br>
-        <p>Consumer Care Team days are Monday-Friday.</p><br>
-        <p>Consumer Care Team times are 7AM - 7PM</p><br>
-        <a href="review.html#faq">FAQ</a>
-      </div>
-      <div class="col-sm-4">
-        <h3><strong>Shop Location</strong></h3><br>
-        <p>Address at:</p>
-        <p><b>Myanmar, Yangon, BarStreet, Ground Floor, No111</b></p>
-        <p>Email us at:</p>
-        <p><b>bakerbakery@.gmail.com</b></p>
-        <p>Call us at:</p>
-        <p><b>+95 9 44131 5589</b></p><br>
-      </div>
-      <div class="col-sm-4">
-        <h3><strong>Google MAP Location</strong></h3><br>
-        <div class="gmap">
-          <iframe style="border:black; border-width:2px; border-style:solid;" width="100%" height="240px" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Myanmar%20Yangon%20Barstreet+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="600" frameborder="0"></iframe>
-        </div>
-      </div>
-    </div>
-
-  </div> <!-- end of contact us -->
-
-  <hr class="fhr">
-  <p style="text-align: center; font-family:Times New Roman; font-size: 16px;">Design and Maintaince by Group-6</p>
-
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"> </script>
 </body>
-
 </html>
---
+++ productdash.php
@@ -0,0 +1,201 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title> Vertical Menu </title>
+    <meta name ="viewport" content ="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src ="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css files/admindashboard.css">
+
+</head>
+<body>
+  <div class='dashboard'>
+    <div class="dashboard-nav">
+        <header><a href="#!" class="menu-toggle">
+            <i>
+            <img src="images/menu.png" style="width:20px; height: 20px;"></i>
+                <span>BAKER BAKERY</span></a>
+        </header>
+
+        <nav class="dashboard-nav-list">
+            <a href="admindashboard.php" class="dashboard-nav-item">
+                <i><img src="images/dashboard.png" style="width:30px; height:30px"></i>
+                DASHBOARD </a>
+
+            <a href="admin_review.php" class="dashboard-nav-item">
+                <i><img src="images/review.png" style="width:30px; height:30px"></i>
+                 REVIEWS </a>
+
+            <a href="products.php" class="dashboard-nav-item">
+                <i class="fas fa-cogs">
+                    <img src="images/product.png" style="width:30px; height:30px">
+                </i> PRODUCTS </a>
+
+            <a href="customers.php" class="dashboard-nav-item active"><i class="fas fa-user">
+                <img src="images/customer.png" style="width:30px; height:30px">
+            </i> CUSTOMERS </a>
+
+            <a href="order.php" class="dashboard-nav-item"><i class="fas fa-user">
+                <img src="images/order.png" style="width:30px; height:30px">
+            </i> ORDERS </a>
+
+            <a href="admin.php" class="dashboard-nav-item"><i class="fas fa-user">
+                <img src="images/admin.png" style="width:30px; height:30px">
+            </i> ADMIN </a>
+
+            <a href="admin_payment.php" class="dashboard-nav-item active"><i class="fas fa-user">
+                <img src="images/payment.png" style="width:30px; height:30px">
+            </i> PAYMENT </a>
+
+            <div class="nav-item-divider"></div>
+            <a href="index.php" class="dashboard-nav-item">
+            <i>
+                <img src="images/logout.png" style="width:30px; height:30px">
+            </i> LOGOUT </a>
+        </nav>
+    </div>
+
+    <div class='dashboard-app'>
+        <header class='dashboard-toolbar'>
+            <a href="#" class="menu-toggle">
+            <img src="images/menu.png" style="width:20px; height: 20px;"> <span style="font-size: 20px; color: black; padding-left: 50px;">Admin</span>
+            </a>
+        </header>
+        <div class='dashboard-content'>
+            <div class='container'>
+                <div class='card'>
+                    <div class='card-header'>
+                        <h1>Admin Panel</h1>
+                    </div>
+
+                    <div class='card-body'>
+                        
+                    <script type="text/javascript">
+                    window.onload = function () {
+
+                    var chart = new CanvasJS.Chart("chartContainer", {
+	                    theme: "light1", // "light2", "dark1", "dark2"
+	                    animationEnabled: true, // change to true		
+	                    title:{
+		                text: "Product Chart"
+	                    },
+	                    data: [
+	                    {
+		            // Change type to "bar", "area", "spline", "pie",etc.
+		                    type: "column",
+		                    dataPoints: [
+
+                                <?php
+                        $counter=0;
+                        include "connection.php";
+                        $viewquery = "SELECT * from product";
+                        foreach ($dbconnection->query($viewquery) as $row) {
+                            $name = $row['pname'];
+                            $Pid = $row['pid'];
+                            $oqty = 0;
+
+                            $viewquery1 = "SELECT * from payment";
+                            foreach ($dbconnection->query($viewquery1) as $row) {
+                                $oid = $row['oid'];
+                                $rmk = $row['remark'];
+
+                                if($rmk=='Confirm'){
+
+                                    $viewquery2 = "SELECT * from order_line where oid='$oid'";
+                                    foreach ($dbconnection->query($viewquery2) as $row) {
+                                        $pqty = $row['qty'];
+                                        $pid = $row['pid'];
+
+                                        if($pid==$Pid){
+                                            $oqty = $oqty + $pqty;
+                                        }
+                                        
+                                    }
+                            }
+
+                            // $viewquery1 = "SELECT * from order_line where pid='$Pid'";
+                            // foreach ($dbconnection->query($viewquery1) as $row) {
+                            //     // $pid = $row['pid'];
+                            //     $pqty = $row['qty'];
+                            //     $oid = $row['oid'];
+
+                            //     $viewquery2 = "SELECT * from payment where oid='$oid'";
+                            //     foreach ($dbconnection->query($viewquery2) as $row) {
+                            //         $rmk = $row['remark'];
+                            //         if($rmk=='Confirm');
+                            //             $oqty = $oqty + $pqty;
+                            //     }
+                                
+                            // }
+
+                            
+                            }
+                            if($oqty!==0){
+                                // echo"<tr>";
+                                // echo"<td> $name </td>";
+                                // echo"<td> $oqty </td>";
+                                // echo" </tr>";
+                                echo"{ label: '$name',  y: $oqty  },";
+                        }
+                    }
+                        ?>
+
+			                // { label: "apple",  y: 10  },
+			                // { label: "orange", y: 15  },
+			                // { label: "banana", y: 25  },
+			                // { label: "mango",  y: 30  },
+			                // { label: "grape",  y: 28  }
+		                ]
+	                }
+	                ]
+                });
+                chart.render();
+
+                }
+                </script>
+
+                        
+
+                                                              
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="bg-light text-center text-lg-start">
+            <!-- Copyright -->
+            <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"> </script>
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+              © 2022 Copyright:
+              <a class="text-dark" href="https://mdbootstrap.com/">Designed and Developed by Group 6</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+    </div>
+</div>  
+<script type="text/javascript">
+    const mobileScreen = window.matchMedia("(max-width: 990px )");
+$(document).ready(function () {
+    $(".dashboard-nav-dropdown-toggle").click(function () {
+        $(this).closest(".dashboard-nav-dropdown")
+            .toggleClass("show")
+            .find(".dashboard-nav-dropdown")
+            .removeClass("show");
+        $(this).parent()
+            .siblings()
+            .removeClass("show");
+    });
+    $(".menu-toggle").click(function () {
+        if (mobileScreen.matches) {
+            $(".dashboard-nav").toggleClass("mobile-show");
+        } else {
+            $(".dashboard").toggleClass("dashboard-compact");
+        }
+    });
+});
+</script>
+</body>
+</html>
